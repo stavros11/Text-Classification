f1fc11c0efb0c6f62682507f5eee624d18002a5f
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-11-10 20:09:47 +0100

    Added some links

--- scrape_trip_advisor.py
+++ scrape_trip_advisor.py
@@ -5,7 +5,9 @@ from utils import trip_advisor_scrapper as tds
 #url = "https://www.tripadvisor.com/Hotel_Review-g1012852-d1519291-Reviews{}-The_Kresten_Royal_Villas_Spa-Kallithea_Rhodes_Dodecanese_South_Aegean.html"
 #url = "https://www.tripadvisor.com/Hotel_Review-g1188087-d2452962-Reviews{}-Stavros_Melathron_Studios-Koskinou_Kallithea_Rhodes_Dodecanese_South_Aegean.html"
 #url = "https://www.tripadvisor.com/Hotel_Review-g776010-d572243-Reviews{}-Lindian_Village-Lardos_Rhodes_Dodecanese_South_Aegean.html"
-url = "https://www.tripadvisor.com/Hotel_Review-g635612-d296796-Reviews{}-Blue_Sea_Beach_Resort-Faliraki_Rhodes_Dodecanese_South_Aegean.html"
+#url = "https://www.tripadvisor.com/Hotel_Review-g635612-d296796-Reviews{}-Blue_Sea_Beach_Resort-Faliraki_Rhodes_Dodecanese_South_Aegean.html"
+#url = "https://www.tripadvisor.com/Hotel_Review-g776010-d557020-Reviews{}-Lindos_Princess_Beach_Hotel-Lardos_Rhodes_Dodecanese_South_Aegean.html"
+url = "https://www.tripadvisor.com/Hotel_Review-g1175546-d2559691-Reviews{}-Princess_Andriana_Resort_Spa-Kiotari_Rhodes_Dodecanese_South_Aegean.html"
 
 scrapper = tds.TripAdvisorReviewScrapper(url)
 
