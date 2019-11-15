b99e17e88a0b739169e97bca8e7db97f7695d4cf
stavros11 <35475381+stavros11@users.noreply.github.com>
2019-11-15 20:21:22 +0100

    Add parser and remove urls

--- scrape_trip_advisor.py
+++ scrape_trip_advisor.py
@@ -1,14 +1,22 @@
+import argparse
 from utils import trip_advisor_scrapper as tds
 
 
-#url = "https://www.tripadvisor.com/Hotel_Review-g1188087-d234314-Reviews{}-Kresten_Palace-Koskinou_Kallithea_Rhodes_Dodecanese_South_Aegean.html"
-#url = "https://www.tripadvisor.com/Hotel_Review-g1012852-d1519291-Reviews{}-The_Kresten_Royal_Villas_Spa-Kallithea_Rhodes_Dodecanese_South_Aegean.html"
-#url = "https://www.tripadvisor.com/Hotel_Review-g1188087-d2452962-Reviews{}-Stavros_Melathron_Studios-Koskinou_Kallithea_Rhodes_Dodecanese_South_Aegean.html"
-#url = "https://www.tripadvisor.com/Hotel_Review-g776010-d572243-Reviews{}-Lindian_Village-Lardos_Rhodes_Dodecanese_South_Aegean.html"
-#url = "https://www.tripadvisor.com/Hotel_Review-g635612-d296796-Reviews{}-Blue_Sea_Beach_Resort-Faliraki_Rhodes_Dodecanese_South_Aegean.html"
-url = "https://www.tripadvisor.com/Hotel_Review-g635613-d252407-Reviews{}-Rodos_Palace-Rhodes_Town_Rhodes_Dodecanese_South_Aegean.html"
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('--url', type=str)
+  parser.add_argument('--max-reviews', type=int, default=None)
 
-scrapper = tds.TripAdvisorReviewScrapper(url)
+  args = parser.parse_args()
+  url = args.url
 
-scrapper.scrape_reviews()
-scrapper.save()
+  # Add {} after Reviews
+  n = url.find("Reviews") + len("Reviews")
+  url = "".join([url[:n], "{}", url[n:]])
+  print("Given url:")
+  print(url)
+  print()
+
+  scrapper = tds.TripAdvisorReviewScrapper(url)
+  scrapper.scrape_reviews(max_reviews=args.max_reviews)
+  scrapper.save()
